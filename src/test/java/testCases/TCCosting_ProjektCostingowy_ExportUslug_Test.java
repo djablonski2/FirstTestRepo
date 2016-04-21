370fd7e551b793d6e33310ed3c98d86c300afa00
Dominik Jabłoński <Dominik Jaboski@192.168.10.189>
2016-04-21 10:57:41 +0200

    qqq

--- src/test/java/testCases/TCCosting_ProjektCostingowy_ExportUslug_Test.java
+++ src/test/java/testCases/TCCosting_ProjektCostingowy_ExportUslug_Test.java
@@ -55,8 +55,7 @@ public class TCCosting_ProjektCostingowy_ExportUslug_Test {
 			CostingProjDetails_Page.btn_ExportToExcel().click();
 			Log.info("Przycisk Export To Excel został kliknięty");
 			Thread.sleep(5000);
-			Thread.sleep(5000);
-
+			
 			LogOut_Action.Execute();
 
 		} catch (Exception e) {
